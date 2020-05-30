9dd85375e2d1fd98b58fdd8b29eb126508fde28d
Meg4Bit <pe4enko111@rambler.ru>
2020-05-30 23:13:29 +0300

    21

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ametapod <pe4enko111@rambler.ru>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/21 11:45:24 by ametapod          #+#    #+#             */
+/*   Updated: 2020/05/21 11:45:24 by ametapod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+
+#endif
