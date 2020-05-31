2e62057b170400f55c1c3e0d1141bf16acb6313c
student <student@iMac-student.local>
2020-05-31 16:41:27 +0300

    add

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,40 @@
 /*   By: ametapod <pe4enko111@rambler.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/21 11:43:45 by ametapod          #+#    #+#             */
-/*   Updated: 2020/05/21 11:43:45 by ametapod         ###   ########.fr       */
+/*   Updated: 2020/05/31 16:40:08 by student          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(int fd, char **line)
+static	int	check_remainder()
 {
+	return (0);
+}
 
-	if (!line)
-		return (-1);
+static	int	submit_line()
+{
+	return (1);
+}
 
+int			get_next_line(int fd, char **line)
+{
+	static	t_list	lst;
+	char			buf[BUFFER_SIZE + 1];
+	int				rt;
+
+	rt = 1;
+	while (check_remainder() < 2 && rt > 0)
+	{
+		if ((rt = read(fd, buf, BUFFER_SIZE)) < 1)
+		{
+			if (check_remainder() == 1)
+				return (submit_line());
+			return (rt);
+		}
+		buf[rt] = 0;
+		*line = buf;
+		ft_lstadd_back(&lst, ft_lstnew());
+	}
+	return (submit_line());
 }
